015c2fb5ff3409293623219268e7c394dddb0025
Артем Бетанов <artembetanov@MacBook-Pro-Artem-2.local>
2020-12-15 16:39:15 +0300

    Initial commit

---
+++ src/com/company/AnyTypeArray.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class AnyTypeArray<E> {
+    private E [] arr;
+
+    public E getArrIndex(int i){
+        return  arr[i] ;
+    }
+
+    public void setArr( E [] arr){
+        this.arr =  arr;
+    }
+
+    public int getLength(){
+        return  arr.length ;
+    }
+
+}
